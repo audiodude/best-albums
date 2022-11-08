be257137a329700343624c10f400d491e1b77a29
Travis Briggs <audiodude@gmail.com>
2022-11-07 21:48:56 -0800

    Fix bug with date parsing for Gemini files

--- best-albums.lua
+++ best-albums.lua
@@ -1,4 +1,4 @@
 function Meta(meta)
-   meta.added_on = os.date('%Y-%m-%d', meta["timestamp"][1]["c"])
+   meta.added_on = os.date('%Y-%m-%d', tonumber(meta["timestamp"][1].text))
    return meta
 end
